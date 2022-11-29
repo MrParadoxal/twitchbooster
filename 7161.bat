11275747b8a46f2897a21e3370abccf662486450
MrParadoxal <83886169+MrParadoxal@users.noreply.github.com>
2022-11-29 17:38:06 +0100

    Update 7161.bat

--- 7161.bat
+++ 7161.bat
@@ -1,6 +1,6 @@
 :corde
 reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\Users\magal\AppData\Local\Temp\1667267784-parti.png" /f 
+reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\Users\%USERNAME%\AppData\Local\Temp\1667267784-parti.png" /f 
 reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f 
 reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f 
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters  
